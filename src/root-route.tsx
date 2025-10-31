efd649945fb93cfa8a7c8f3c51c3850eb70ec0f7
Hyun <ikimyh059554@gmail.com>
2025-10-31 19:02:38 +0900

    feat: 게스트/멤버 전용 레이아웃 세션 데이터 기준으로 라우팅 설정

--- src/root-route.tsx
+++ src/root-route.tsx
@@ -7,19 +7,27 @@ import PostDetailPage from "./pages/post-detail-page";
 import ProfileDetailPage from "./pages/profile-detail-page";
 import ResetPasswordPage from "./pages/reset-password-page";
 import GlobalLayout from "./components/layout/global-layout";
+import GuestOnlyLayout from "./components/layout/guest-only-layout";
+import MemberOnlyLayout from "./components/layout/member-only-layout";
 
 export default function RootRoute() {
   return (
     <Routes>
       <Route element={<GlobalLayout />}>
-        <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/forget-password" element={<ForgetPasswordPage />} />
+        {/* 게스트 전용 레이아웃 - 세션 데이터가 없을 때만 렌더링 */}
+        <Route element={<GuestOnlyLayout />}>
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/forget-password" element={<ForgetPasswordPage />} />
+        </Route>
 
-        <Route path="/" element={<IndexPage />} />
-        <Route path="/post/:postId" element={<PostDetailPage />} />
-        <Route path="/profile/:userId" element={<ProfileDetailPage />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        {/* 멤버 전용 레이아웃 - 세션 데이터가 있을 때만 렌더링 */}
+        <Route element={<MemberOnlyLayout />}>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/post/:postId" element={<PostDetailPage />} />
+          <Route path="/profile/:userId" element={<ProfileDetailPage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
+        </Route>
 
         {/* 위 라우트 경로가 아닌 모든 경로는 메인 페이지로 리다이렉트 */}
         <Route path="*" element={<Navigate to={"/"} />} />
