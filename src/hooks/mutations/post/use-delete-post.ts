ff21eb532352bf582f9417ad1dfdaf03499497f4
Hyun <ikimyh059554@gmail.com>
2025-11-04 18:28:31 +0900

    feat: 포스트 crud 기능 구현 및 supabase rsa 적용

--- src/hooks/mutations/post/use-delete-post.ts
+++ src/hooks/mutations/post/use-delete-post.ts
@@ -1,9 +1,12 @@
 import { deleteImageInPath } from "@/api/image";
 import { deletePost } from "@/api/post";
+import { QUERY_KEYS } from "@/lib/constants";
 import type { UseMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useDeletePost(callbacks?: UseMutationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: deletePost,
     onSuccess: async (deletedPost) => {
@@ -13,6 +16,9 @@ export function useDeletePost(callbacks?: UseMutationCallback) {
       if (deletedPost.image_urls && deletedPost.image_urls.length > 0) {
         await deleteImageInPath(`${deletedPost.author_id}/${deletedPost.id}`);
       }
+
+      // 포스트 삭제 시, 포스트 목록 캐시 무효화 (invalidateQueries -> resetQueries 로 변경 이유: 무한 스크롤 시 데이터 손실 방지)
+      queryClient.resetQueries({ queryKey: QUERY_KEYS.post.list });
     },
     onError: (error) => {
       if (callbacks?.onError) callbacks.onError(error);
